7398b6391e9c78996db71396dbeb096108b02d0d
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-02-15 15:09:45 -0700

    Problem 13 complete.

--- lab.js
+++ lab.js
@@ -220,7 +220,20 @@ console.log(garfield.name)
 
 //Code here
 
-//////////////////////////// PROBLEM 14 ////////////////////////////
+class Wizard {
+  constructor(name, age, favoriteSpell) {
+    this.name = name;
+    this.age = age;
+    this.favoriteSpell = favoriteSpell
+  }
+  castSpell() {
+    console.log(`${this.name} has cast ${this.favoriteSpell}`)
+  }
+}
+let gandalf = new Wizard(`Gandalf`, 3000, `Flame of Udun`)
+gandalf.castSpell()
+
+/////////////////////////// PROBLEM 14 ////////////////////////////
 /*
     Write a class called Phone. We'll use it as if we were creating
     phone objects to keep track of inventory using an app.
